e8d5ede1e7057d51e2b240298482ad55aeb3d67c
Techchauhan <88832979+Techchauhan@users.noreply.github.com>
2024-12-07 21:26:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+  // Create a text node with the string "DOM load success"
+  const message = document.createTextNode("DOM load success");
+
+  // Append the text node to the body
+  document.body.appendChild(message);
+});
